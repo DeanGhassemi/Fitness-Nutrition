865cb0ea1f86c4a9c89d98ca582a392c041e6c43
Dean <deang467@gmail.com>
2022-11-10 12:17:12 -0500

    Workout Context

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,12 +2,39 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutContext = CreateContext()
 
+/* 
+    State is the previous state before making the change which is the workouts
+    under the state hook.
+
+    This keeps the state in sync with the database
+ */
+export const WorkoutReducer = (state, action) => {
+    switch (action.type){
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUTS':
+            return {
+                workouts: [action.payload, ...state.work]
+            }
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        default:
+            return state
+    }
+}
+
 export const WorkoutContextProvider = ({ children }) => {
 
-    const [state, dispatch] = useReducer(workoutsReducer, {workouts:null})
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts:null
+    })
 
     return (
-        <WorkoutContext.Provider value={p}>
+        <WorkoutContext.Provider value={{state, dispatch}}>
             {children}
         </WorkoutContext.Provider>
     )
